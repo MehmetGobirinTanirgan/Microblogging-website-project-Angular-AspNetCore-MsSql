730cd224429821b188c0fdee411013baf0f016be
Mehmet Gobirin TANIRĞAN <mehmet.g.tanirgan@gmail.com>
2021-08-12 02:03:37 +0300

    Logout visual issue fixed

--- TwitterWEB/src/app/home/home.component.ts
+++ TwitterWEB/src/app/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
     private authService: AuthenticationService
   ) {}
 
-  tweets: TweetModel[] = [];
+  tweets: TweetModel[] = [] as TweetModel[];
   ngOnInit(): void {
     this.getAllTweets();
   }
